c5fbc1ec631aee999a5fcc2f93f239e516e5f45b
Jamison Fox <fox.jamison@outlook.com>
2023-03-17 14:24:12 -0600

    removed unused dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducer from './reducers';
